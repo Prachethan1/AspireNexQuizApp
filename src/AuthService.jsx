44fbbefc184d42d710c67e0766b9cc980bc1123d
prachethan <prachethan@email.com>
2024-07-02 20:59:16 +0530

    second

--- src/AuthService.jsx
+++ src/AuthService.jsx
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 // const API_BASE_URL = "http://localhost:9192/api";
-const API_BASE_URL = "https://aspirenexquizserver-production.up.railway.app/api"
+const API_BASE_URL = "https://enthusiastic-gentleness-production.up.railway.app/api"
+//https://aspirenexquizserver-production.up.railway.app/
 
 class AuthService {
     register(user) {
